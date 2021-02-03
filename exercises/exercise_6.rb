51127a09200658e80fbe6ead3b5a88a6cc3dbdde
Michael Xiang <yorkmike4@gmail.com>
2021-02-03 09:50:42 +0000

    finish

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+  has_many :employees
+end
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "first", last_name: "1", hourly_rate: 61)
+@store2.employees.create(first_name: "second", last_name: "2", hourly_rate: 62)
+@store2.employees.create(first_name: "third", last_name: "3", hourly_rate: 63)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :hourly_rate, :numericality => {:greater_than_or_equal_to =>40}
+  validates :hourly_rate, :numericality => {:less_than_or_equal_to =>200}
+end
+class Store
+  validates :name, :length => {:minimum => 3}
+  validates :annual_revenue, :numericality => {:only_integer => true, :minimum => 0}
+end
+
+input = gets.chomp
+
+test = Store.create(name: input)
+puts test.errors.full_messages
